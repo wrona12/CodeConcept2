81bcd57ff7576858817e82f3475e2506e180b443
Wronatsky <bartlomiejgawron@wp.pl>
2021-03-06 17:19:20 +0100

    Kosmetyka

--- src/main/java/com/codeconcept/DAO/GeoDAO.java
+++ src/main/java/com/codeconcept/DAO/GeoDAO.java
@@ -12,38 +12,28 @@ import java.net.URL;
 public class GeoDAO {
 
     public String request(String endpoint) throws Exception {
+
         StringBuilder sb = new StringBuilder();
+
 //        "http://resources.codeconcept.pl/api/distance/"
         URL url = new URL(endpoint);
-
-//        Otwarcie połączenia do URL.
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        try {
-//        Czyta tylko bity.
-            InputStream inputStream = urlConnection.getInputStream();
 
-//        Wczytuje podobnie jak InputStream, ale do bufora, czyli do lokacji w pamięci.
+        try {
+            InputStream inputStream = urlConnection.getInputStream();
             BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
 
-//        Odczytanie jako znaki.
             InputStreamReader inputStreamReader = new InputStreamReader(bufferedInputStream);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-//        Czytanie jednej lini na raz.
             String inputLine = bufferedReader.readLine();
             while (inputLine != null) {
-
-//            Dodanie do wyjścia.
                 sb.append(inputLine);
-
-//            Czytanie kolejnej linii.
                 inputLine = bufferedReader.readLine();
-
             }
         } finally {
             urlConnection.disconnect();
         }
-
         return sb.toString();
     }
 }

7062f7792958e13f5c39c99ad711fc617b61d02e
Wronatsky <bartlomiejgawron@wp.pl>
2021-03-07 13:50:18 +0100

    Headers: 200 status, 400 status.

--- src/main/java/com/codeconcept/controller/GeoDAOController.java
+++ src/main/java/com/codeconcept/controller/GeoDAOController.java
@@ -6,6 +6,8 @@ import com.codeconcept.service.TimeResponse;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Duration;
@@ -24,7 +26,10 @@ public class GeoDAOController {
     }
 
     @GetMapping("/time/walk/{source1}/{destination1}")
-    public TimeResponse getWalkTime(@PathVariable String source1, @PathVariable String destination1) throws Exception {
-        return geoService.getWalkTime(source1, destination1);
+    public ResponseEntity<TimeResponse> getWalkTime(@PathVariable String source1, @PathVariable String destination1) throws Exception {
+        if(geoService.getWalkTime(source1, destination1)==null){
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(geoService.getWalkTime(source1, destination1), HttpStatus.OK);
     }
 }
